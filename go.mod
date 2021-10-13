d857863f1d44f1ffd86f357c4363d4c50788def0
devuser <pythoner@icloud.com>
2021-10-13 23:58:26 +0800

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/devuser/datatypes
+
+go 1.16
+
+require (
+	github.com/jinzhu/now v1.1.2
+	github.com/sirupsen/logrus v1.8.1
+	gorm.io/datatypes v1.0.2
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/driver/postgres v1.1.2
+	gorm.io/driver/sqlite v1.1.6
+	gorm.io/driver/sqlserver v1.1.0
+	gorm.io/gorm v1.21.16
+)
---
+++ untBizdate.go
@@ -0,0 +1,60 @@
+package datatypes
+
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: devuser@gmail.com
+ * @Wechat: PythonBJ
+ * @Mobile: 13911223211
+ * @Date: 2021-10-13 22:53:56
+ * @LastEditors: devuser@gmail.com
+ * @LastEditTime: 2021-10-13 23:39:24
+ */
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Bizdate time.Time
+
+func (date *Bizdate) Scan(value interface{}) (err error) {
+	nullTime := &sql.NullTime{}
+	err = nullTime.Scan(value)
+	*date = Bizdate(nullTime.Time)
+	return
+}
+
+func (date Bizdate) Value() (driver.Value, error) {
+	y, m, d := time.Time(date).Date()
+	return time.Date(y, m, d, 0, 0, 0, 0, time.Time(date).Location()), nil
+}
+
+// GormDataType gorm common data type
+func (date Bizdate) GormDataType() string {
+	return "datetime"
+}
+
+func (date Bizdate) GobEncode() ([]byte, error) {
+	return time.Time(date).GobEncode()
+}
+
+func (date *Bizdate) GobDecode(b []byte) error {
+	return (*time.Time)(date).GobDecode(b)
+}
+
+func (date Bizdate) MarshalJSON() ([]byte, error) {
+	v := time.Time(date).Format("2006-01-02")
+	log.Println(v)
+	return []byte(v), nil
+}
+
+func (date *Bizdate) UnmarshalJSON(b []byte) error {
+	// return (*time.Time)(date).UnmarshalJSON(b)
+	t, err := time.ParseInLocation("2006-01-02", string(b), time.Local)
+	*date = Bizdate(t)
+	return err
+}
